890b9f8b2b447088be6c92d9ac04a19aca787b8b
Mel Heisey <melanieheisey@mail.weber.edu>
2026-03-19 11:36:53 -0600

    create animal and dog class, manually test in main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from animal import Animal
+from dog import Dog
 
 if __name__ == "__main__":
-    # TODO: Create an instance of the Animal class
-    # TODO: Print the Animal instance
-    # TODO: Call the method to make a generic animal sound
+    my_animal = Animal(name="Gerald-Herald", species="Lawn Gnome")
+    print(my_animal)
+    my_animal.speak()
 
-    # TODO: Create an instance of the Dog class
-    # TODO: Print the Dog instance
-    # TODO: Call the method to make the dog-specific sound
+    my_dog = Dog(name="Sir Fluffington", breed="Cavochon Hybrid")
+    print(my_dog)
+    my_dog.speak()
 
-    # TODO print out all the animals
+    print(Animal.all_animals)  # This will show all animals created, including the dog since it's a subclass of Animal
+else:
+    print("This code is meant to be run as a script, not imported as a module.")
