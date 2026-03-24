1fb83f98a8a3a12ad7bfe60d299f9eb5a7dcc152
Mel Heisey <melanieheisey@mail.weber.edu>
2026-03-24 12:00:24 -0600

    Make a cat class (in class)

--- cat.py
+++ cat.py
@@ -1,15 +1,15 @@
 from animal import Animal
-class Dog(Animal):
+class Cat(Animal):
     """
     Derived class representing a dog, which is a type of Animal.
     """
 
-    def __init__(self, name, species="Dog", breed=None):
+    def __init__(self, name, species="Cat", breed=None):
         self.breed = breed
         super().__init__(name, species)
     
     def speak(self):
-        print("The dog barks.")
+        print("The cat meows.")
 
     def __str__(self):
         if self.breed:
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from animal import Animal
 from dog import Dog
+from cat import Cat
 
 if __name__ == "__main__":
     my_animal = Animal(name="Gerald-Herald", species="Lawn Gnome")
@@ -9,6 +10,10 @@ if __name__ == "__main__":
     my_dog = Dog(name="Sir Fluffington", breed="Cavochon Hybrid")
     print(my_dog)
     my_dog.speak()
+
+    my_cat = Cat(name="Pityr", breed="Russian Blue")
+    print(my_cat)
+    my_cat.speak()
 
     print(Animal.all_animals)  # This will show all animals created, including the dog since it's a subclass of Animal
 else:
